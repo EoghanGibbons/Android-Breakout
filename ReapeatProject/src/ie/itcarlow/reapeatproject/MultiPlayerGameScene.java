24719f9ee9bd4223a31a0ec97093765eb3b3f2f3
EoghanGibbons <eoghanfrommayo@live.com>
2015-08-18 06:45:14 +0100

    Added Websockets and multiplayer game scene

---
+++ ReapeatProject/src/ie/itcarlow/reapeatproject/MultiPlayerGameScene.java
@@ -0,0 +1,41 @@
+package ie.itcarlow.reapeatproject;
+
+import ie.itcarlow.reapeatproject.SceneManager.SceneType;
+
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.Scene;
+import org.andengine.input.touch.TouchEvent;
+
+public class MultiPlayerGameScene extends BaseScene implements IOnSceneTouchListener {
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createScene() {
+		
+		
+	}
+
+	@Override
+	public void onBackKeyPressed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SceneType getSceneType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void disposeScene() {
+		// TODO Auto-generated method stub
+		
+	}
+		
+
+}
